2cf6d2906e242832ae3034558498b1d618d05f23
ALIF-AL-RAZI <103985669+ALIF-AL-RAZI@users.noreply.github.com>
2022-09-16 20:42:02 +0600

    Update whatsapp auto message testing.py

--- whatsapp auto message testing.py
+++ whatsapp auto message testing.py
@@ -1,8 +1,8 @@
 import pywhatkit
 
 for x in range(4): #automatically send 4 messages
-    #pywhatkit.sendwhatmsg('+8801872792494', 'hi', 19, 33)  #in sendwhatmsg() 19 is hour and 33 is minutes. that means in 19:33 it will send meassage
-    pywhatkit.sendwhatmsg_instantly('+8801872792494','hi', 7, True,2) # Here it will send messages instantly after running codes. true is used close the tab which was opened automatically after runnning code.
+    #pywhatkit.sendwhatmsg("'''The mobile number where you want send meassages. example: '''0123456789", 'hi', 19, 33)  #in sendwhatmsg() 19 is hour and 33 is minutes. that means in 19:33 it will send meassage
+    pywhatkit.sendwhatmsg_instantly("'''The mobile number where you want send meassages. example: '''0123456789",'hi', 7, True,2) # Here it will send messages instantly after running codes. true is used close the tab which was opened automatically after runnning code.
 
 
 
